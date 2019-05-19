7c88e8ca51ac1fd9172c7da4280efa743dbcaad5
Bimesh De Silva <bimeshde@gmail.com>
2019-05-18 21:15:46 -0400

    Added build files

---
+++ frontend/fancam-generator/build/precache-manifest.10b0832a04d6d55bce01e68673e461c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a34e15f3d3623297a4d52252c94a914",
+    "url": "/fancam-generator/index.html"
+  },
+  {
+    "revision": "ec7488bd6915156b5976",
+    "url": "/fancam-generator/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/fancam-generator/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ec7488bd6915156b5976",
+    "url": "/fancam-generator/static/js/main.26843609.chunk.js"
+  },
+  {
+    "revision": "3a6eec646b0602753d68",
+    "url": "/fancam-generator/static/js/runtime~main.aea3702f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fancam-generator/static/media/logo.5d5d9eef.svg"
+  }
+]);
